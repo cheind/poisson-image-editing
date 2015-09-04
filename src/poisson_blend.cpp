9e68927736849c47e12c9f792c09822cb0ca65e0
Christoph Heindl <christoph.heindl@gmail.com>
2015-09-04 12:48:45 +0200

    Initial Commit

---
+++ src/poisson_blend.cpp
@@ -0,0 +1,26 @@
+/**
+   This file is part of PoissonBlend.
+
+   Copyright Christoph Heindl 2015
+
+   PoissonBlend is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   PoissonBlend is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with PoissonBlend.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <blend/poisson_blend.h>
+
+namespace blend {
+
+
+}
